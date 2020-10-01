a5b9eba330f6cda3155e0e07383b7bb746eed89d
Kohei Imanishi <imanishi@missinglink.co.jp>
2020-10-01 09:17:23 +0900

    add demo app

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo';
+  images = [
+    { src: 'https://picsum.photos/seed/1/800/600' },
+    { src: 'https://picsum.photos/seed/2/600/800' },
+    { src: 'https://picsum.photos/seed/3/800/800' },
+    { src: 'https://picsum.photos/seed/4/800/600' },
+    { src: 'https://picsum.photos/seed/5/600/800' },
+    { src: 'https://picsum.photos/seed/6/800/800' },
+    { src: 'https://picsum.photos/seed/7/800/600' },
+    { src: 'https://picsum.photos/seed/8/600/800' },
+    { src: 'https://picsum.photos/seed/9/800/800' },
+    { src: 'https://picsum.photos/seed/10/800/600' },
+    { src: 'https://picsum.photos/seed/11/600/800' },
+    { src: 'https://picsum.photos/seed/12/800/800' },
+  ];
 }
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PhotoGalleryModule } from '@twogate/ngx-photo-gallery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PhotoGalleryModule.forRoot({ defaultOptions: { showHideOpacity: true } })],
   providers: [],
   bootstrap: [AppComponent],
 })
