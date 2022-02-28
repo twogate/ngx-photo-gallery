13be9e09d17046c7c670f6541857775a7c3f221c
Shunta KARASAWA <karasawa@twogate.com>
2022-02-28 13:54:00 +0900

    Update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,14 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
-    project: ['./tsconfig.json', 'projects/core/tsconfig.lib.json', 'projects/core/tsconfig.lib.prod.json', 'projects/core/tsconfig.spec.json', 'projects/demo/tsconfig.app.json', 'projects/demo/tsconfig.spec.json'],
+    project: [
+      './tsconfig.json',
+      'projects/core/tsconfig.lib.json',
+      'projects/core/tsconfig.lib.prod.json',
+      'projects/core/tsconfig.spec.json',
+      'projects/demo/tsconfig.app.json',
+      'projects/demo/tsconfig.spec.json',
+    ],
   },
   rules: {
     '@angular-eslint/component-selector': ['error', { type: 'element', style: 'kebab-case' }],
